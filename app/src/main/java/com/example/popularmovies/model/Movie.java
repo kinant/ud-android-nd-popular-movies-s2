5bef775b2988db40908b995767b102925770a68d
Kinan T <kinan.t@gmail.com>
2020-03-18 12:02:13 -0600

    Data Model
    Crated model for a movie. This class will be used to store a movie's data.

---
+++ app/src/main/java/com/example/popularmovies/model/Movie.java
@@ -0,0 +1,17 @@
+package com.example.popularmovies.model;
+
+public class Movie {
+    private String title;
+    private String releaseDate;
+    private String poster;
+    private String vote_average;
+    private String plot;
+
+    public Movie(String title, String releaseDate, String poster, String vote_avg, String plot){
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.poster = poster;
+        this.vote_average = vote_avg;
+        this.plot = plot;
+    }
+}
