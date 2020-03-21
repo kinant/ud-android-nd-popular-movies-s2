1576ee6e3f069e54c24800d8bdaae027e549c79b
Kinan T <kinan.t@gmail.com>
2020-03-21 17:55:26 -0600

    Commenting 1
    Commented MovieAdapter

--- app/src/main/java/com/example/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/popularmovies/MovieAdapter.java
@@ -14,29 +14,59 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+/**
+ * {@link MovieAdapter} exposes a list of weather forecasts to a
+ * {@link androidx.recyclerview.widget.RecyclerView}
+ */
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
 
+    // List of movies that will be populated (set) once loaded from the API
     private List<Movie> mMovieData;
 
+    /*
+     * An on-click handler that we've defined to make it easy for an Activity to interface with
+     * our RecyclerView. Used when clicking on a movie poster.
+     */
     private final MovieAdapterOnClickHandler mClickHandler;
 
+    /**
+     * The interface that receives onClick messages.
+     */
     public interface MovieAdapterOnClickHandler {
         void onClick(Movie movie);
     }
 
+    /**
+     * Creates a MovietAdapter.
+     *
+     * @param clickHandler The on-click handler for this adapter. This single handler is called
+     *                     when an item is clicked.
+     */
     public MovieAdapter(MovieAdapterOnClickHandler clickHandler){
         mClickHandler = clickHandler;
     }
 
+    /**
+     * Cache of the children views for a Movie Poster grid item.
+     */
     public class MovieAdapterViewHolder extends RecyclerView.ViewHolder implements OnClickListener {
         public final ImageView mMoviePoster;
 
         public MovieAdapterViewHolder(View view){
             super(view);
+
+            // get reference to the image view that will show the poster
             mMoviePoster = (ImageView) view.findViewById(R.id.image_iv);
+
+            // set the on click listener to this child view
             view.setOnClickListener(this);
         }
 
+        /**
+         * This gets called by the child views during a click.
+         *
+         * @param v The View that was clicked
+         */
         @Override
         public void onClick(View view) {
             int adapterPosition = getAdapterPosition();
@@ -45,6 +75,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapter
         }
     }
 
+    /**
+     * This gets called when each new ViewHolder is created. This happens when the RecyclerView
+     * is laid out. Enough ViewHolders will be created to fill the screen and allow for scrolling.
+     *
+     * @param viewGroup The ViewGroup that these ViewHolders are contained within.
+     * @param viewType  Not used since our RecyclerView does not use more than one item
+     * @return A new MovieAdapterViewHolder that holds the View for each list item
+     */
     @Override
     public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
@@ -57,16 +95,32 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapter
         return new MovieAdapterViewHolder(view);
     }
 
+    /**
+     * OnBindViewHolder is called by the RecyclerView to display the data at the specified
+     * position. In this method, we update the contents of the ViewHolder to display the poster
+     * image for a particular position, using the "position" argument that is conveniently
+     * passed into us.
+     *
+     * @param movieAdapterViewHolder The ViewHolder which should be updated to represent the
+     *                                  contents of the item at the given position in the data set.
+     * @param position                  The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(MovieAdapterViewHolder movieAdapterViewHolder, int position) {
-        // Set the image
-
-        // https://guides.codepath.com/android/Displaying-Images-with-the-Picasso-Library
+        /** Set the image using Picasso:
+        * Using code from: https://guides.codepath.com/android/Displaying-Images-with-the-Picasso-Library
+        */
         Picasso.get()
                 .load(mMovieData.get(position).getPoster())
                 .into(movieAdapterViewHolder.mMoviePoster);
     }
 
+    /**
+     * This method simply returns the number of items to display. It is used behind the scenes
+     * to help layout our Views and for animations.
+     *
+     * @return The number of items available in our forecast
+     */
     @Override
     public int getItemCount() {
         if (mMovieData == null){
@@ -76,6 +130,13 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapter
         return mMovieData.size();
     }
 
+    /**
+     * This method is used to set the movie data on a MovieAdapter if we've already
+     * created one. This is handy when we get new data from the web but don't want to create a
+     * new MovieAdapter to display it.
+     *
+     * @param movies The list of movies to be displayed on the grid.
+     */
     public void setMovieData(List<Movie> movies){
         mMovieData = movies;
         notifyDataSetChanged();
